90a7f1586c2e746f67059f49400d1400f5f74118
Priyanshu Mohan Ginkal <71839009+Priyanshumg@users.noreply.github.com>
2022-09-17 22:59:21 +0530

    Add files via upload

---
+++ HangMan/hackerank.py
@@ -0,0 +1,20 @@
+# # Enter your code here. Read input from STDIN. Print output to STDOUT
+# total_students_english = int(input())
+# roll_number_english = set(input().split())
+#
+# total_students_french = int(input())
+# roll_number_french = set(input().split())
+#
+# print(len(roll_number_english.symmetric_difference(roll_number_french)))
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# elements_in_a = int(input())
+# elements_in_a_list = set(input().split())
+# number_of_sets = int(input())
+# num = 0
+# while num < number_of_sets:
+#     num += 1
+#     command, number = set(input().split())
+#     if command == "intersection_update":
+#         update = elements_in_a_list.intersection_update(set(number))
+#         print(update)
